d33b0597ac5381eaa419490335ccf8dd3d3a7003
Fellipe Leandro <faugusto12@gmail.com>
2017-11-09 16:20:56 -0300

    Update pngPro.asm

--- arquitetura/pngPro.asm
+++ arquitetura/pngPro.asm
@@ -38,17 +38,58 @@ iglobal
                      OnCreate = FormOnCreate,                   \
                      Caption = 'Digital Image Processing App'
 
-           ObjTemplate tfChild, TImageLabel, imageForm,  \
-                       x = 16, y = 64,            \
+         ObjTemplate tfChild, TImageLabel, imageForm,  \
+                       x = 8, y = 80,            \
                        width = 410, height = 440,   \
                        Visible = TRUE,           \
                        Image = [img],       \
                        Caption = 'LabelForm'
-          ObjTemplate tfChild or tfEnd, TButton, btnNegative, \
+         ObjTemplate  tfChild, TTreeView, tvFolders,  \
+                     x=450,y=200,\
+                     width = 200,height = 200,\
+                     SplitCell = SplitTest.cellTree,            \
+                     Visible = TRUE
+         ObjTemplate tfChild,TEdit,img1Name,\
+                     x=32,y=8,\
+                     width=152,height = 24,\
+                     Visible=TRUE
+         ObjTemplate tfChild,TEdit,img2Name,\
+                    x=32,y=40,\
+                    width=152,height=24,\
+                    Visible=TRUE
+         ObjTemplate tfChild,TButton,btnInsertImg1,\
+                    x=192,y=8,\
+                    width=88,height=24,\
+                    Visible=TRUE,\
+                    OnClick = insertImg1Proc,\
+                    Caption='Insert Img 1'
+
+         ObjTemplate tfChild,TButton,btnInsertImg2,\
+                   x=192,y=40,\
+                   width=88,height=24,\
+                   Visible=TRUE,\
+                   OnClick=insertImg2Proc,\
+                   Caption='Insert Img 2'
+
+
+         ObjTemplate tfChild or tfEnd, TButton, btnNegative, \
                       x=550, y=70, \
-                      width = 120, height = 32,\
+                      width = 120, height = 20,\
                       Visible = TRUE, \
                       Caption = 'Negativo'
+
+
+SplitStart SplitTest
+
+  Split stVert or stJustGap , 4, 24, 16, 48
+    Split stHoriz or stOriginBR or stJustGap, 4, 64, 48, 200
+      Cell cellEdit
+      Cell cellButton
+
+    Cell cellTree
+
+SplitEnd
+
 
 
 
@@ -63,6 +104,8 @@ uglobal
   aClickCount dd ?
   textToCopy        dd ?
   img               dd ?
+  img1Dir           dd ?
+  img2Dir           dd ?
 
 endg
 
@@ -72,7 +115,11 @@ aCaption2 text 'UFRN - 2017'
 warningCopy text 'Nada Digitado'
 char text 'a'
 image file 'jubs.png'
+
+
 sizeof.image = $ - image
+image2 file 'pic.png'
+sizeof.image2=$-image2
 
 
 proc FormOnCreate as TObject.OnCreate
@@ -85,6 +132,27 @@ begin
         mov [img],eax
         return
 
+endp
+
+proc insertImg1Proc, .self,.button
+begin
+        push eax
+       ; exec img1Name, TEdit:GetText ;Get Text
+       ; mov [img1Dir],eax
+        getfile img0,"pic.png"
+        stdcall CreateImagePNG,img0,img0.size
+        mov [img],eax
+
+;        set [img2Name],TEdit:Text,eax
+
+       set [imageForm],TImageLabel:Image,[img]
+
+        pop eax
+        return
+endp
+proc insertImg2Proc,.self,.button
+begin
+return
 endp
 
 ; Main Program
@@ -115,3 +183,4 @@ start:
         stdcall TerminateAll    ; from the stack
 
 ; end of main program
+                     
